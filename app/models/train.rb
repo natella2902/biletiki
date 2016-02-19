8b24c7566968d296ad73c705bf51012099e5bc74
natella <n.jelonkina@mail.ru>
2016-02-19 22:10:27 +0300

    add assoc

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,6 @@
 class Train < ActiveRecord::Base
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   has_many :tickets
+  belongs_to :route
   validates :number, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218180138) do
+ActiveRecord::Schema.define(version: 20160219183345) do
 
   create_table "passengers", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160218180138) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "current_station_id"
+    t.integer  "route_id"
   end
 
 end
